3e444ad8e6f4e525ff6e539fb76dddb1b6e4f154
devsapariya94 <devsapariya94@gmail.com>
2022-09-12 11:02:26 +0530

    12-9-2022

---
+++ 12-9-2022/Residency_main.java
@@ -0,0 +1,26 @@
+abstract class Residency{
+    String name;
+    int rNumber;
+     int area;
+     int unit_rate;
+    public Residency(){
+        name=null;
+        rNumber=0;
+        area=0;
+        unit_rate=0;
+    }
+
+    Residency(String name; int rNumber, int area, int unit_rate){
+        this.name=name;
+        this.rNumber=rNumber;
+        this.area=area;
+        this.unit_rate=unit_rate;
+    }
+}
+
+
+
+
+public class Residency_main {
+    
+}
