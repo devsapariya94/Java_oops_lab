954e1af565ad4a2d9cff06c04161d0614e33b323
Dev Sapariya <devsapariya94@gmail.com>
2022-10-29 17:34:04 +0530

    small change

--- 19-10-2022/Test.java
+++ 19-10-2022/Test.java
@@ -1,6 +1,18 @@
 //write .dat file
 public class Test{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ioException{
+        File file = new File("test.dat");
+        if(file.exists()){
+            System.out.println("File already exists");
+            System.exit(0);
+        }
+        try(
+            DataOutputStream output = new DataOutputStream(new FileOutputStream(file));
+        ){
+            
+        }
+    }
+
 
 
     }
